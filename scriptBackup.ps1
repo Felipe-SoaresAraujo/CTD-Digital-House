2a011740409b08694d5f89aa26c5cd4c50cea7db
Felipe Soares de Araujo <69880898+Felipe-SoaresAraujo@users.noreply.github.com>
2021-08-26 16:10:45 -0300

    Update scriptBackup.ps1

--- scriptBackup.ps1
+++ scriptBackup.ps1
@@ -11,6 +11,7 @@ $destino = New-Item -Path C:\ex_backup\Backup -ItemType Directory
 #para copiar os arquivos para a nova pasta
 Copy-Item $origem -Destination $destino -Recurse -Verbose
 
+#insere a data atual
 $date = Get-Date -Format "dd-MM-yyyy HH-mm-ss UTC_-03-00"
 
 Get-ChildItem $destino -Force #exibe a pasta copiada
